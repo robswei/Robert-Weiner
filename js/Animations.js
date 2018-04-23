827ddd9fa2fc3c09ad57cf76f243062b793f375f
Robert Weiner <rtweine2@illinois.edu>
2018-04-23 15:28:25 -0500

    Refined particles

--- js/Animations.js
+++ js/Animations.js
@@ -1,171 +1,171 @@
 window.sr = ScrollReveal();
 
-    //Opening Animations
-    sr.reveal('.name', {
-      duration:3000,
-      origin: 'bottom'
-    });
-    sr.reveal('.sum', {
-      duration:3000,
-      origin: 'bottom',
-      distance: '100px'
-    });
-    sr.reveal('.abt', {
-      duration: 2000,
-      origin: 'top',
-    });
-    sr.reveal('.hranim', {
-      duration: 2000,
-      origin: 'bottom',
-      distance: '100px'
-    });
-    sr.reveal('.abtText', {
-      duration: 2000,
-      origin: 'bottom',
-      distance: '100px'
-    });
+//Opening Animations
+sr.reveal(".name", {
+  duration: 3000,
+  origin: "bottom"
+});
+sr.reveal(".sum", {
+  duration: 3000,
+  origin: "bottom",
+  distance: "100px"
+});
+sr.reveal(".abt", {
+  duration: 2000,
+  origin: "top"
+});
+sr.reveal(".hranim", {
+  duration: 2000,
+  origin: "bottom",
+  distance: "100px"
+});
+sr.reveal(".abtText", {
+  duration: 2000,
+  origin: "bottom",
+  distance: "100px"
+});
 
-    //Experience Animations
-    sr.reveal('.apple', {
-      duration: 2000,
-      origin: 'bottom'
-    });
-    sr.reveal('.park', {
-      duration: 2000,
-      origin: 'bottom'
-    });
-    sr.reveal('.irobot', {
-      duration: 2000,
-      origin: 'top',
-      viewFactor: 0.75
-    });
-    sr.reveal('.mrdc', {
-      duration: 2000,
-      origin: 'top',
-      viewFactor: 0.75
-    });
-    sr.reveal('.hranim1', {
-      duration: 2000,
-      origin: 'bottom',
-      distance: '300px'
-    });
-    sr.reveal('.rob15', {
-      duration: 2000,
-      origin: 'bottom',
-      distance: '300px',
-    });
-    sr.reveal('.hranim2', {
-      duration: 2000,
-      origin: 'bottom',
-      distance: '300px',
-      viewFactor: 0.75
-    });
-    sr.reveal('.rob16', {
-      duration: 2000,
-      delay: 250,
-      origin: 'bottom',
-      distance: '300px',
-    });
-    sr.reveal('.hranim3', {
-      duration: 2000,
-      origin: 'bottom',
-      distance: '300px',
-      viewFactor: 0.75
-    });
-    sr.reveal('.rob17', {
-      duration: 3000,
-      delay: 500,
-      origin: 'bottom',
-      distance: '300px',
-    });
-    sr.reveal('.makers', {
-      duration: 2000,
-      origin: 'top',
-      viewFactor: 0.75
-    });
-    sr.reveal('.creative', {
-      duration: 2000,
-      origin: 'top',
-      viewFactor: 0.75
-    });
-    sr.reveal('.hranim4', {
-      duration: 2000,
-      origin: 'bottom',
-      distance: '300px',
-    });
-    sr.reveal('.quad', {
-      duration: 2000,
-      origin: 'bottom',
-      distance: '300px'
-    });
-    sr.reveal('.quadpic', {
-      duration: 2000,
-      origin: 'bottom',
-      distance: '300px'
-    });
+//Experience Animations
+sr.reveal(".apple", {
+  duration: 2000,
+  origin: "bottom"
+});
+sr.reveal(".park", {
+  duration: 2000,
+  origin: "bottom"
+});
+sr.reveal(".irobot", {
+  duration: 2000,
+  origin: "top",
+  viewFactor: 0.75
+});
+sr.reveal(".mrdc", {
+  duration: 2000,
+  origin: "top",
+  viewFactor: 0.75
+});
+sr.reveal(".hranim1", {
+  duration: 2000,
+  origin: "bottom",
+  distance: "300px"
+});
+sr.reveal(".rob15", {
+  duration: 2000,
+  origin: "bottom",
+  distance: "300px"
+});
+sr.reveal(".hranim2", {
+  duration: 2000,
+  origin: "bottom",
+  distance: "300px",
+  viewFactor: 0.75
+});
+sr.reveal(".rob16", {
+  duration: 2000,
+  delay: 250,
+  origin: "bottom",
+  distance: "300px"
+});
+sr.reveal(".hranim3", {
+  duration: 2000,
+  origin: "bottom",
+  distance: "300px",
+  viewFactor: 0.75
+});
+sr.reveal(".rob17", {
+  duration: 3000,
+  delay: 500,
+  origin: "bottom",
+  distance: "300px"
+});
+sr.reveal(".makers", {
+  duration: 2000,
+  origin: "top",
+  viewFactor: 1
+});
+sr.reveal(".creative", {
+  duration: 2000,
+  origin: "top",
+  viewFactor: 1
+});
+sr.reveal(".hranim4", {
+  duration: 2000,
+  origin: "bottom",
+  distance: "300px"
+});
+sr.reveal(".quad", {
+  duration: 2000,
+  origin: "bottom",
+  distance: "300px"
+});
+sr.reveal(".quadpic", {
+  duration: 2000,
+  origin: "bottom",
+  distance: "300px"
+});
 
-    //Projects Animation
-    sr.reveal('.pred', {
-      duration: 2000,
-      delay: 500,
-      origin: 'bottom',
-      distance: '500px'
-    });
-    sr.reveal('.leftButton', {
-      duration: 2000,
-      delay: 500,
-      origin: 'bottom',
-      distance: '300px'
-    });
-    sr.reveal('.rightButton', {
-      duration: 2000,
-      delay: 750,
-      origin: 'bottom',
-      distance: '300px'
-    });
-    sr.reveal('.tetris', {
-      duration: 2000,
-      delay: 250,
-      origin: 'bottom',
-      distance: '500px'
-    });
-    sr.reveal('.gitButton1', {
-      duration: 2000,
-      delay: 250,
-      origin: 'bottom',
-      distance: '300px'
-    });
-    sr.reveal('.mrdcrobots', {
-      duration: 2000,
-      origin: 'bottom',
-      distance: '500px'
-    });
-    sr.reveal('.gitButton2', {
-      duration: 2000,
-      origin: 'bottom',
-      distance: '300px'
-    });
+//Projects Animation
+sr.reveal(".pred", {
+  duration: 2000,
+  delay: 500,
+  origin: "bottom",
+  distance: "500px"
+});
+sr.reveal(".leftButton", {
+  duration: 2000,
+  delay: 500,
+  origin: "bottom",
+  distance: "300px"
+});
+sr.reveal(".rightButton", {
+  duration: 2000,
+  delay: 750,
+  origin: "bottom",
+  distance: "300px"
+});
+sr.reveal(".tetris", {
+  duration: 2000,
+  delay: 250,
+  origin: "bottom",
+  distance: "500px"
+});
+sr.reveal(".gitButton1", {
+  duration: 2000,
+  delay: 250,
+  origin: "bottom",
+  distance: "300px"
+});
+sr.reveal(".mrdcrobots", {
+  duration: 2000,
+  origin: "bottom",
+  distance: "500px"
+});
+sr.reveal(".gitButton2", {
+  duration: 2000,
+  origin: "bottom",
+  distance: "300px"
+});
 
-    //Connect Animations
-    sr.reveal('.cont', {
-      duration: 2000,
-      origin: 'top',
-      viewFactor: 0.75
-    });
-    sr.reveal('.linked', {
-      duration: 1000,
-      origin: 'bottom',
-      distance: '100px'
-    });
-    sr.reveal('.git', {
-      duration: 1000,
-      delay: 250,
-      origin: 'bottom',
-      distance: '100px'
-    });
-    sr.reveal('.mail', {
-      duration: 1000,
-      delay: 500,
-      origin: 'bottom',
-      distance: '100px'
-    });
+//Connect Animations
+sr.reveal(".cont", {
+  duration: 2000,
+  origin: "top",
+  viewFactor: 0.75
+});
+sr.reveal(".linked", {
+  duration: 1000,
+  origin: "bottom",
+  distance: "100px"
+});
+sr.reveal(".git", {
+  duration: 1000,
+  delay: 250,
+  origin: "bottom",
+  distance: "100px"
+});
+sr.reveal(".mail", {
+  duration: 1000,
+  delay: 500,
+  origin: "bottom",
+  distance: "100px"
+});
